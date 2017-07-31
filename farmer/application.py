c847160caa76383949448fd0855cc47387a4b306
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-07-31 12:48:09 +0300

    Enable events for all new workers periodically

--- farmer/application.py
+++ farmer/application.py
@@ -1,14 +1,38 @@
 import logging
 
+import threading
+import time
+
+from celery import Celery
+
+import control
+
 logging.basicConfig(level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
+
+ENABLE_EVENTS_POLL_TIME = 1
 
 
 class Farmer(object):
     def __init__(self, broker_url):
         self.broker_url = broker_url
-        pass
+        self.celery_app = Celery(broker=self.broker_url)
+
+        self.enable_events_thread = threading.Thread(target=self._enable_events)
 
     def start(self):
         logger.info("Starting Farmer with broker %s" % self.broker_url)
+        self.enable_events_thread.start()
+
+    def stop(self):
+        logger.info("Stopping farmer")
+        self.enable_events_thread.running = False
+
+    def _enable_events(self):
+        while getattr(threading.currentThread, "running", True):
+            try:
+                control.enable_events(self.celery_app)
+            finally:
+                logger.debug("Sleeping for %i seconds before enabling events again" % ENABLE_EVENTS_POLL_TIME)
+                time.sleep(ENABLE_EVENTS_POLL_TIME)
---
+++ farmer/control.py
@@ -0,0 +1,2 @@
+def enable_events(celery_app):
+    celery_app.control.enable_events()
