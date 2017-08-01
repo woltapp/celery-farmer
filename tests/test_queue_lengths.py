2870723ea4017bd36223c1c3231d15af5a2b8d51
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-08-01 12:53:57 +0300

    Send heartbeats from queue_lengths thread

--- tests/test_queue_lengths.py
+++ tests/test_queue_lengths.py
@@ -10,19 +10,32 @@ from tests import fixtures
 from tests.helpers.wait import wait_until_success
 
 
+@patch('celery.app.control.Inspect.active_queues')
 class QueueLengthsTestCase(unittest.TestCase):
-    @patch('celery.app.control.Inspect.active_queues')
+
+    def setUp(self):
+        self.statsd_mock = Mock()
+        self.queue_lengths = QueueLengths(
+            Celery(broker="redis://localhost"),
+            statsd_client=self.statsd_mock,
+            poll_time=0.1,
+        )
+
     def test_tracks_counts_of_events(self, active_queues):
         active_queues.return_value = fixtures.active_queues_response
 
-        statsd_mock = Mock()
-        queue_lengths = QueueLengths(
-            Celery(broker="redis://localhost"),
-            statsd_client=statsd_mock,
-            poll_time=0.1,
-        )
         try:
-            queue_lengths.start()
-            wait_until_success(lambda: self.assertTrue(statsd_mock.gauge.called))
+            self.queue_lengths.start()
+            wait_until_success(lambda: self.assertTrue(self.statsd_mock.gauge.called))
         finally:
-            queue_lengths.stop()
+            self.queue_lengths.stop()
+
+    def test_sends_heartbeats(self, active_queues):
+        active_queues.return_value = fixtures.active_queues_response
+
+        try:
+            self.queue_lengths.start()
+            wait_until_success(lambda: self.assertTrue(self.statsd_mock.incr.called))
+            self.assertEqual(self.statsd_mock.incr.call_args[0][0], "heartbeats.queue_lengths")
+        finally:
+            self.queue_lengths.stop()
