90c6c8277872e6bb861a145920d035c76f1aab5b
Atte Lautanala <atte.lautanala@wolt.com>
2020-05-27 13:20:23 +0300

    Add Python 3.8 to supported languages

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     entry_points=dict(
